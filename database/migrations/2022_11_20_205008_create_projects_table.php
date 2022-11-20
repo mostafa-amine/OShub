c385b8b91c37f9d9f8816b1ac9408ca736cdad4e
mostafa amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2022-11-20 21:57:27 +0100

    Initial commit

---
+++ database/migrations/2022_11_20_205008_create_projects_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique();
+            $table->string('slug');
+            $table->string('short_description');
+            $table->string('project_url')->unique()->nullable();
+            $table->string('repo_url')->unique();
+            $table->string('packagist_url')->nullable();
+            $table->text('description');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
