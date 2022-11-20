de46a5673d0cc6a22681f738a921c202ba88d93f
mostafa amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2022-11-20 22:03:07 +0100

    Add User & project Relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function Projects()
+    {
+        return $this->hasMany(Project::class);
+    }
 }
