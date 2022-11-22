a17c85bad06a5029f6d80602ecfe6330178a7a81
mostafa amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2022-11-22 19:25:04 +0100

    Add app layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.app')
+<x-layouts.app title="Register">
 
-@section('content')
+<x-slot:content>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -70,4 +70,5 @@
         </div>
     </div>
 </div>
-@endsection
+</x-slot:content>
+</x-layouts.app>
