cddc12e2f61781e08e2c58c8a2750fad2012366a
mostafa amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2022-11-25 10:41:36 +0100

    Add User profile Link

--- resources/views/components/layouts/navbar.blade.php
+++ resources/views/components/layouts/navbar.blade.php
@@ -35,12 +35,19 @@
                     <li class="nav-item">
                         <a class="btn btn-success" href="{{ route('submit-project') }}">{{ __('Submit Project') }}</a>
                     </li>
+
+
                     <li class="nav-item dropdown">
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }}
+                            {{ Auth::user()->user_name }}
                         </a>
 
+
                         <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('profile' , ['username' => Auth::user()->user_name]) }}" >
+                                Your Profile
+                            </a>
+
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
@@ -50,7 +57,10 @@
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                 @csrf
                             </form>
+
                         </div>
+
+
                     </li>
                 @endguest
             </ul>
