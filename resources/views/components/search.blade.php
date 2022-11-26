86276d0c71e86f0d29b72b7258c91bae1c500303
mostafa amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2022-11-26 11:25:34 +0100

    Make search component

---
+++ resources/views/components/search.blade.php
@@ -0,0 +1,10 @@
+<div class="mt-5">
+    <div class="container">
+            <form action="">
+                <div class="search-form d-flex justify-content-center">
+                    <input placeholder="search for projects" type="search" name="search" class="form-control rounded-0">
+                    <input type="submit" class="btn btn-success rounded-0" value="Search">
+                </div>
+            </form>
+    </div>
+</div>
