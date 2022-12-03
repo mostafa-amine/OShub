7e6f89d42c6c7bb2babba182a0f11d9fc6203c07
mostafa amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2022-12-03 11:02:52 +0100

    :card_file_box: Update user factory

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Project;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -14,10 +16,18 @@ class ProjectFactory extends Factory
      *
      * @return array<string, mixed>
      */
+    protected $model = Project::class;
     public function definition()
     {
         return [
-            //
+            'title' => fake()->text(20),
+            'slug' => fake()->slug(),
+            'short_description' => fake()->text(100),
+            'project_url' => fake()->url(),
+            'repo_url' => fake()->url(),
+            'packagist_url' => fake()->url(),
+            'description' => fake()->text(300),
+            'user_id' => User::factory()->has(Project::factory()->count(3))
         ];
     }
 }
