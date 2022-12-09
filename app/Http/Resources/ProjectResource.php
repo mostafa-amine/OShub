4049b64727ad0ec96f7056ee9eb1a7225009eda9
mostafa amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2022-12-09 23:40:00 +0100

    :memo: Create API ProjectRessource

---
+++ app/Http/Resources/ProjectResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProjectResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+
+    // Just For Learning
+    /**
+     * Database attributes
+    */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'user' => new UserRessource($this->user)
+        ];
+    }
+}
