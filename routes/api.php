1f062bfb9fae0c67f9734bb0e3205b5bae9048c7
mostafa amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2022-12-09 23:38:58 +0100

    :technologist: Project API Endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\ProjectController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/projects' , [ProjectController::class , 'index']);
+Route::get('/projects/{project:slug}' , [ProjectController::class , 'show']);
+
+// Show All Users
+Route::get('/users' , [UserController::class , 'index']);
