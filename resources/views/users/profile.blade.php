4912178aa305f1285384b6cace5318ac21ce0f0a
mostafa amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2022-12-15 15:40:28 +0100

    :sparkles: Add livewire component

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -1,7 +1,7 @@
 <x-layouts.app title="{{ $user->username() . ' (' .$user->name() . ')' }}">
 
     <x-slot:content>
-
+        <livewire:profile.user-profile :user="$user"/>
     </x-slot:content>
 
 
